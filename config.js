3625525d8a2f0bedf135e8ce8e4179cd2099fa03
vishswat11011 <sati.v1011@gmail.com>
2022-02-25 12:03:58 +0530

    facebook auth with nodejs passport ejs

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+	'facebookAuth': {
+		'clientID':  '264051189227588', // your App ID
+		'clientSecret':  '02d6e7b82de4e1bc8cde0915f7408198', // your App Secret
+		'callbackURL':  'http://localhost:3000/auth/facebook/callback'
+	}
+}
